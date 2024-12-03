6cd1bf451779905f7300e876a88d02044bf1e0d0
Rozpi <100348581+Rozpide@users.noreply.github.com>
2024-12-03 05:51:49 +0000

    añado codigo en window.load

--- src/app.js
+++ src/app.js
@@ -6,6 +6,22 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
+  let pronombres = ["the", "our"];
+  let adjetivos = ["great", "big"];
+  let sustantivos = ["jogger", "racoon"];
+  let dominios = [".io", ".com", ".es"];
   //write your code here
   console.log("Hello Rigo from the console!");
+  for (let i = 0; i < pronombres.length; i++) {
+    for (let j = 0; j < adjetivos.length; j++) {
+      for (let k = 0; k < sustantivos.length; k++) {
+        for (let h = 0; h < dominios.length; h++) {
+          /*console.log(`${pronombres}${adjetivos}${sustantivos}${dominios}`);*/
+          console.log(
+            pronombres[i] + adjetivos[j] + sustantivos[k] + dominios[h]
+          );
+        }
+      }
+    }
+  }
 };
